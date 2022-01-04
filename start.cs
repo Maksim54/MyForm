4a37636c8ac5057fdd5b58836ff9fc93591f3960
Maksim54 <77334211+Maksim54@users.noreply.github.com>
2022-01-04 14:55:52 +0200

    Add files via upload

---
+++ start.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVorm
+{
+    class start:System.Windows.Forms.Form
+    {
+        public start()
+        {
+            Button Start_btn = new Button
+            {
+                Text = "minu oma aken",
+                Location = new System.Drawing.Point(10, 10)
+            };
+            Start_btn.Click += Start_btn_Click;
+            this.Controls.Add(Start_btn);
+            Button start_btn_2 = new Button
+            {
+                Text = "Veel aken",
+                Location = new System.Drawing.Point(200, 10)
+            };
+            start_btn_2.Click += Start_btn_2_Click;
+            this.Controls.Add(start_btn_2);
+        }
+
+        private void Start_btn_2_Click(object sender, EventArgs e)
+        {
+            MyForm uus_aken = new MyForm(5,8);
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            MyForm uus_aken = new MyForm("Ma olen ilus õun","Vali midagi","üks","kaks","kolm","neli");
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }
+    }
+}
